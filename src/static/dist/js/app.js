1022ceb251386e7786c51de44254f3bee9acb04a
Matt Scott <matt@azorian.solutions>
2023-02-10 21:20:41 -0500

    Working on label maker app.

--- src/static/dist/js/app.js
+++ src/static/dist/js/app.js
@@ -2,24 +2,6 @@ jQuery.validator.addMethod("label", function (value, element) {
     return this.optional(element) || /^[\w\s\(\)\.\-\$#]+$/i.test(value);
 }, "Labels may only contain letters, numbers, spaces, and these characters: ( ) . - # $");
 
-jQuery.validator.addMethod("zipcode", function (value, element) {
-    return this.optional(element) || /^\d{5}(?:-\d{4})?$/.test(value);
-}, "Please provide a valid zipcode.");
-
-jQuery.validator.addMethod("macaddress", function (value, element) {
-    return this.optional(element) || /^([0-9A-F]{2}[:-]?){5}([0-9A-F]{2})$/i.test(value);
-}, "Please provide a valid MAC address.");
-
-jQuery.validator.addMethod("ipv4address", function (value, element) {
-    let regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/i
-    return this.optional(element) || regex.test(value);
-}, "Please provide a valid IPv4 address.");
-
-jQuery.validator.addMethod("ipv6prefix", function (value, element) {
-    let regex = /^(?:(?:(?:[A-F0-9]{1,4}:){5}[A-F0-9]{1,4}|(?:[A-F0-9]{1,4}:){4}:[A-F0-9]{1,4}|(?:[A-F0-9]{1,4}:){3}(?::[A-F0-9]{1,4}){1,2}|(?:[A-F0-9]{1,4}:){2}(?::[A-F0-9]{1,4}){1,3}|[A-F0-9]{1,4}:(?::[A-F0-9]{1,4}){1,4}|(?:[A-F0-9]{1,4}:){1,5}|:(?::[A-F0-9]{1,4}){1,5}|:):(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])|(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}|(?:[A-F0-9]{1,4}:){6}:[A-F0-9]{1,4}|(?:[A-F0-9]{1,4}:){5}(?::[A-F0-9]{1,4}){1,2}|(?:[A-F0-9]{1,4}:){4}(?::[A-F0-9]{1,4}){1,3}|(?:[A-F0-9]{1,4}:){3}(?::[A-F0-9]{1,4}){1,4}|(?:[A-F0-9]{1,4}:){2}(?::[A-F0-9]{1,4}){1,5}|[A-F0-9]{1,4}:(?::[A-F0-9]{1,4}){1,6}|(?:[A-F0-9]{1,4}:){1,7}:|:(?::[A-F0-9]{1,4}){1,7}|::)(?:\/[0-9]{2})$/i
-    return this.optional(element) || regex.test(value);
-}, "Please provide a valid IPv6 prefix.");
-
 let numberOnlyNormalizer = function (value) {
     return $.trim(value.replace(/\D+/g, ''))
 }
@@ -37,4 +19,103 @@ let getRootElement = function (el, selector) {
 
 let validateErrorPlacement = function (error, element) {
     error.appendTo(element.parents('.form-group'))
+}
+
+let LabelModel = function(data) {
+    // Create a reference to this for use within callbacks
+    let self = this
+
+    // Define defaults
+    let defaults = {
+        technologies: ['Fixed', 'Mobile'],
+        id: null,
+        provider_name: null,
+        service_plan_name: null,
+        technology: null,
+        intro_price: null,
+        intro_period: null,
+        monthly_price: null,
+        contract_length: null,
+        contract_length_unit: null,
+        contract_terms_url: null,
+        monthly_fees: [],
+        one_time_fees: [],
+        early_termination_fee: null,
+        discounts_url: null,
+        discounts: [],
+        acp: null,
+        download_speed: null,
+        download_speed_unit: 'Mbps',
+        upload_speed: null,
+        upload_speed_unit: 'Mbps',
+        latency: null,
+        latency_unit: 'ms',
+        data_included: null,
+        data_included_unit: null,
+        additional_data_charge: null,
+        additional_data_amount: null,
+        additional_data_unit: null,
+        network_management_policy_url: null,
+        privacy_policy_url: null,
+        support_url: null,
+        support_phone: null,
+        support_email: null
+    }
+
+    // Merge defaults with runtime data
+    data = $.extend(defaults, data)
+
+    // Initialize model properties
+    self.technologies = ko.observableArray(data.technologies)
+    self.id = ko.observable(data.id)
+    self.provider_name = ko.observable(data.provider_name)
+    self.service_plan_name = ko.observable(data.service_plan_name)
+    self.technology = ko.observable(data.technology)
+    self.intro_price = ko.observable(data.intro_price)
+    self.intro_period = ko.observable(data.intro_period)
+    self.monthly_price = ko.observable(data.monthly_price)
+    self.contract_length = ko.observable(data.contract_length)
+    self.contract_length_unit = ko.observable(data.contract_length_unit)
+    self.contract_terms_url = ko.observable(data.contract_terms_url)
+    self.monthly_fees = ko.observableArray(data.monthly_fees)
+    self.one_time_fees = ko.observableArray(data.one_time_fees)
+    self.early_termination_fee = ko.observable(data.early_termination_fee)
+    self.discounts_url = ko.observable(data.discounts_url)
+    self.discounts = ko.observableArray(data.discounts)
+    self.acp = ko.observable(data.acp)
+    self.download_speed = ko.observable(data.download_speed)
+    self.download_speed_unit = ko.observable(data.download_speed_unit)
+    self.upload_speed = ko.observable(data.upload_speed)
+    self.upload_speed_unit = ko.observable(data.upload_speed_unit)
+    self.latency = ko.observable(data.latency)
+    self.latency_unit = ko.observable(data.latency_unit)
+    self.data_included = ko.observable(data.data_included)
+    self.data_included_unit = ko.observable(data.data_included_unit)
+    self.additional_data_charge = ko.observable(data.additional_data_charge)
+    self.additional_data_amount = ko.observable(data.additional_data_amount)
+    self.additional_data_unit = ko.observable(data.additional_data_unit)
+    self.network_management_policy_url = ko.observable(data.network_management_policy_url)
+    self.privacy_policy_url = ko.observable(data.privacy_policy_url)
+    self.support_url = ko.observable(data.support_url)
+    self.support_phone = ko.observable(data.support_phone)
+    self.support_email = ko.observable(data.support_email)
+
+    self.monthly_price_formatted = ko.computed(function() {
+        return '$' + self.monthly_price();
+    }, self);
+
+    self.early_termination_fee_formatted = ko.computed(function() {
+        return '$' + self.early_termination_fee();
+    }, self);
+
+    self.support_phone_formatted = ko.computed(function() {
+        area = self.support_phone().substr(0, 3);
+        prefix = self.support_phone().substr(3, 3);
+        suffix = self.support_phone().substr(6, 4);
+        return '(' + area + ') ' + prefix + '-' + suffix;
+    }, self);
+
+    self.support_phone_url = ko.computed(function() {
+        return 'tel:' + self.support_phone();
+    }, self);
 }
